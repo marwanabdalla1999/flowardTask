9d53c9ada1309df161e2616cf5ecc5e8121f3809
marwan <marwanabdalla1999@gmail.com>
2025-07-28 05:23:56 +0300

    hot fix

--- app/src/main/java/com/floward/floward_task/core/ui/navigations/NavHost.kt
+++ app/src/main/java/com/floward/floward_task/core/ui/navigations/NavHost.kt
@@ -1,18 +1,10 @@
 package com.floward.floward_task.core.ui.navigations
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
-import com.floward.floward_task.features.dashboard.DashboardScreen
 import com.floward.floward_task.features.dashboard.dashboardRoute
-import com.floward.floward_task.features.profile.ProfileScreen
-import com.floward.floward_task.features.profile.ProfileViewModel
 import com.floward.floward_task.features.profile.profileRoute
 
 
@@ -27,7 +19,13 @@ fun AppNavHost(
         startDestination = DashboardDestination
     ) {
         dashboardRoute { message ->
-            navController.navigate(ProfileDestination(feedbackMessage = message))
+            navController.navigate(ProfileDestination(feedbackMessage = message)){
+                popUpTo(navController.graph.startDestinationId) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
         }
 
         profileRoute()
